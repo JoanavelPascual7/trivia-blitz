85148c69ebdf709ea79d287938d8d8e07746bdef
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-03-12 15:03:04 -0400

    some of it is working

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// window.onload = () => {
-//     let rightAnswer = document.getElementsByClassName("right-answer")[0]
-//     let wrongAnswer = document.getElementsByClassName("wrong-answer")[0]
-//     rightAnswer.onclick = rightAnswerClick
-//     wrongAnswer.onclick = wrongAnswerClick
-// }
+window.onload = () => {
+    let rightAnswer = document.getElementsByClassName("right-answer")[0];
+    let wrongAnswer = document.getElementsByClassName("wrong-answer")[0];
+    let answerElements = document.querySelectorAll(".answers-box li");
+    rightAnswer.onclick = rightAnswerClick;
+    wrongAnswer.onclick = wrongAnswerClick;
+    for (let i = 0; i < answerElements.length; i++) {
+        answerElements[i].onclick = answerClick;
+    }
+};
 
-// let rightAnswerClick = (event) => {
-//     event.target.classList.add("green")
-// }
+let rightAnswerClick = (event) => {
+    event.target.classList.add("green");
+};
 
-// let wrongAnswerClick = (event) => {
-//     event.target.classList.add("red")
-// }
+let wrongAnswerClick = (event) => {
+    event.target.classList.add("red");
+};
+
+let answerClick = (event) => {
+    if (event.target.classList.contains("right-answer")) {
+        event.target.classList.add("green");
+    } else if (event.target.classList.contains("wrong-answer")) {
+        event.target.classList.add("red");
+    }
+};
